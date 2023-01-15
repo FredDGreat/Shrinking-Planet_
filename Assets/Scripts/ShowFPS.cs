0384f7a60995ed00881086d385b945c08b2b0066
Frederick Eberechi Onwukwe <FredDGreat1@gmail.com>
2023-01-15 23:00:46 +0100

    Update ShowFPS.cs

--- Assets/Scripts/ShowFPS.cs
+++ Assets/Scripts/ShowFPS.cs
@@ -8,6 +8,11 @@ public class ShowFPS : MonoBehaviour
 {
     public float timer, refresh, avgFrameRate;
     public TextMeshProUGUI fpsText;
+
+    [SerializeField] private Text _fpsText;
+    [SerializeField] private float _hudRefreshRate = 1f;
+
+    private float _timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +22,17 @@ public class ShowFPS : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float timeLapse = Time.smoothDeltaTime;
+        /*float timeLapse = Time.smoothDeltaTime;
         timer = timer <= 0 ? refresh : timer -= timeLapse;
 
         if (timer <= 0) avgFrameRate = (int)(1f / timeLapse);
-        fpsText.text = avgFrameRate.ToString();
+        fpsText.text = avgFrameRate.ToString();*/
+
+        if (Time.unscaledTime > _timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.text = "" + fps;
+            _timer = Time.unscaledTime + _hudRefreshRate;
+        }
     }
 }
